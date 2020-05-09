dfba8262f4897d41d54e04552c1e8e1c65d16b6f
Anderson Fuentes UA <62313759+andersonfuentesUA@users.noreply.github.com>
2020-05-08 21:46:35 -0500

    Primera version de mi codigo en Java.

---
+++ proyectobasico01/proyectobasico01/src/main/java/Repositorio/programa01.java
@@ -0,0 +1,12 @@
+
+package Repositorio;
+
+
+public class programa01 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hola, soy Anderson Fuentes Pinedo");
+    }
+    
+}
