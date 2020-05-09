cecf202eb06836f272df0523c57485eefd620413
Anderson Fuentes <afuentesp@autonoma.edu.pe>
2020-05-08 22:12:57 -0500

    Segunda modificación código Java desde Github.
    Aquí estamos modificando el código Java desde la misma cuenta de Github.

--- proyectobasico01/proyectobasico01/src/main/java/Repositorio/programa01.java
+++ proyectobasico01/proyectobasico01/src/main/java/Repositorio/programa01.java
@@ -7,6 +7,7 @@ public class programa01 {
     
     public static void main(String[] args) {
         System.out.println("Hola, soy Anderson Fuentes Pinedo");
+        System.out.println("Estoy agregando esta linea de codigo desde Github");
     }
     
 }
